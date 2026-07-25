49947e0c9d1505779c162026083a12f52ea208af
Md Amirul Hoda Polash <mdamirulhodapolash@gmail.com>
2026-07-25 23:07:05 +0600

    feat: add launch button interaction

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// PolashOS Main JavaScript
+document.addEventListener("DOMContentLoaded", () => {
+
+    const button = document.getElementById("launchBtn");
+
+    button.addEventListener("click", () => {
+
+        button.innerText = "Initializing Polaris Engine...";
+
+        button.disabled = true;
+
+        setTimeout(() => {
+
+            button.innerText = "Boot Experience Coming Soon 🚀";
+
+        }, 1800);
+
+    });
+
+});
